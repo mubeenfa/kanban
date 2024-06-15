a1199c2d8e5e4a2c74d5d2ab26f8a4bc75e74b19
mubeenfa <mubeenf.ahd@gmail.com>
2024-06-15 18:19:08 +0500

    designed task details side panel, it contains: - Title field - Status drop down - Due date (with End date option) - Estimate field - Priority drop down - Description field

--- main.js
+++ main.js
@@ -2,37 +2,52 @@ const btn_AddTask = document.querySelector(".add-button");
 const btn_EditTask = document.querySelector(".edit-task");
 const btn_DeleteTask = document.querySelector(".delete-button");
 
-const inp_Task = document.getElementById("input-task")
+const inp_Task = document.getElementById("input-task");
 
 const todoParent = document.getElementById("todo-drop-area");
 const inProgressParent = document.getElementById("inprogress-drop-area");
 const completeParent = document.getElementById("complete-drop-area");
 
+let currentTaskStatus = "Todo";
 
 const boardData = [
   {
-    "colId": 1,
-    "colName": "ToDo",
-    "tasks": []  
+    colId: 1,
+    colName: "ToDo",
+    tasks: [],
   },
   {
-    "colId": 2,
-    "colName": "In Progress",
-    "tasks": []  
+    colId: 2,
+    colName: "In Progress",
+    tasks: [],
   },
   {
-    "colId":3,
-    "colName": "Complete",
-    "tasks": []  
-  }
-]
+    colId: 3,
+    colName: "Complete",
+    tasks: [],
+  },
+];
+
+const taskDetails = {
+  taskTitle: "",
+  dueDate: "",
+  endDate: "",
+  estimates: 0,
+  priority: "",
+  taskDescription: "",
+};
 
 let counter = 1;
+
+function ToggleDarkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
 btn_AddTask.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if(inp_Task.value != "") {
+  if (inp_Task.value != "") {
     GenerateTaskSlab(inp_Task.value, counter);
 
     InsertData(counter, inp_Task.value);
@@ -44,7 +59,7 @@ btn_AddTask.addEventListener("click", (e) => {
 function GenerateTaskSlab(taskTitle, id) {
   const newTask = document.createElement("div");
   newTask.classList = "task-card";
-  newTask.id = `${"task_"+id}`;
+  newTask.id = `${"task_" + id}`;
   newTask.draggable = true;
 
   const inputTask = document.createElement("input");
@@ -67,16 +82,16 @@ function GenerateTaskSlab(taskTitle, id) {
   newTask.appendChild(actionButtons);
   newTask.appendChild(inputTask);
 
-  editButton.addEventListener("click", function(event) {
-    Edit(event)
+  editButton.addEventListener("click", function (event) {
+    Edit(event);
   });
 
-  deleteButton.addEventListener("click", function(event) {
-    Delete(event)
+  deleteButton.addEventListener("click", function (event) {
+    Delete(event);
   });
 
-  inputTask.addEventListener("focusout", function(event) {
-    EditTaskTitleOnChange(event)
+  inputTask.addEventListener("focusout", function (event) {
+    EditTaskTitleOnChange(event);
   });
 
   newTask.addEventListener("dragstart", () => {
@@ -90,8 +105,10 @@ function GenerateTaskSlab(taskTitle, id) {
   todoParent.appendChild(newTask);
 }
 
+// ============== Manage Task ===========
 function Edit(e) {
-  const inputField = e.target.parentElement.parentElement.querySelector('input');
+  const inputField =
+    e.target.parentElement.parentElement.querySelector("input");
   inputField.disabled = false;
 
   const colId = e.target.parentElement.parentElement;
@@ -100,52 +117,47 @@ function Edit(e) {
 function Delete(e) {
   const task = e.target.parentElement.parentElement;
   const colId = CheckColumn(task.parentElement);
-  const id = parseInt(task.id.split("_")[1])
+  const id = parseInt(task.id.split("_")[1]);
 
-  DeleteData (task, colId, id);
+  DeleteData(task, colId, id);
 }
 
-function EditTaskTitleOnChange(e){
-  const inputField = e.target.parentElement.parentElement.querySelector('input');
+function OpenTask(e) {
+  const task = e.target.parentElement.parentElement;
+  const colId = CheckColumn(task.parentElement);
+  const id = parseInt(task.id.split("_")[1]);
+}
+
+function EditTaskTitleOnChange(e) {
+  const inputField =
+    e.target.parentElement.parentElement.querySelector("input");
   const taskId = inputField.parentElement.id.split("_");
   const columnEle = inputField.parentElement.parentElement;
-  
+
   let colId = CheckColumn(columnEle.id);
 
-
-  // if(columnEle.id === "todo-drop-area") {
-  //   colId = 1;
-  // }
-  // else if(columnEle.id === "inprogress-drop-area") {
-  //   colId = 2;
-  // }
-  // else if(columnEle.id === "complete-drop-area") {
-  //   colId = 3;
-  // }
-  
   inputField.disabled = true;
   UpdateTaskTitle(colId, taskId[1], inputField.value);
 }
 
 function CheckColumn(col) {
   let colId;
-  if(col.id === "todo-drop-area") {
+  if (col.id === "todo-drop-area") {
     colId = 1;
-  }
-  else if(col.id === "inprogress-drop-area") {
+  } else if (col.id === "inprogress-drop-area") {
     colId = 2;
-  }
-  else if(col.id === "complete-drop-area") {
+  } else if (col.id === "complete-drop-area") {
     colId = 3;
   }
 
   return colId;
 }
+// ======================================
 
-
+// ========== Task CRUD ===============
 function DeleteData(taskDiv, colId, taskId) {
   // Find the column with the given colId
-  const column = boardData.find(col => col.colId === colId);
+  const column = boardData.find((col) => col.colId === colId);
 
   // Check if the column was found
   if (column) {
@@ -154,7 +166,7 @@ function DeleteData(taskDiv, colId, taskId) {
     // console.log('Original tasks:', taskArray);
 
     // Filter out the task with the specified taskId
-    const updatedTaskArray = taskArray.filter(task => {
+    const updatedTaskArray = taskArray.filter((task) => {
       // console.log(`Checking task with id ${task.id}`);  // Debug: Log task IDs being checked
       return task.id !== taskId;
     });
@@ -167,9 +179,8 @@ function DeleteData(taskDiv, colId, taskId) {
     if (taskDiv) {
       taskDiv.remove();
     }
-  }
-  else {
-    console.log('Column not found');
+  } else {
+    console.log("Column not found");
   }
 
   // const taskArray = column.tasks;
@@ -181,20 +192,119 @@ function DeleteData(taskDiv, colId, taskId) {
   // console.log(boardData);
 }
 
-function InsertData(taskId, task){
+function InsertData(taskId, task) {
   let newTask = {
-    "id":taskId,
-    "task":task
-   };
+    id: taskId,
+    task: task,
+  };
 
-   boardData[0].tasks.push(newTask);
-   counter++;
+  boardData[0].tasks.push(newTask);
+  counter++;
 }
 
-function UpdateTaskTitle(colId, taskId, task){
-   if(boardData[colId].tasks.id === taskId) {
+function UpdateTaskTitle(colId, taskId, task) {
+  if (boardData[colId].tasks.id === taskId) {
     boardData[colId].tasks.task = task;
-   }
-   
-   console.log(JSON.stringify(boardData, null, 2));
+  }
+
+  console.log(JSON.stringify(boardData, null, 2));
 }
+// ======================================
+
+// ========== Manage Task Details ==============
+const dueDateToggle = document.getElementById("duedatetoggle");
+const endDate = document.getElementById("enddate");
+const startdate = document.getElementById("startdate");
+const taskDueDate = document.getElementById("taskduedate");
+
+const ddButtons = document.querySelectorAll(".priority-button");
+const taskPriorityButton = document.getElementById("task-priority-button");
+const ddStatusButtons = document.querySelectorAll(".status-button");
+const taskStatusButton = document.getElementById("task-status-button");
+
+dueDateToggle.addEventListener("change", (e) => {
+  if (e.currentTarget.checked) {
+    endDate.classList.remove("enddate-active");
+    taskDueDate.style.width = "100%";
+    startdate.style.width = "50%";
+    endDate.style.width = "50%";
+  } else {
+    endDate.classList.add("enddate-active");
+    taskDueDate.style.width = "50%";
+    startdate.style.width = "100%";
+    endDate.style.width = "100%";
+  }
+});
+
+function PriorityDropDownBtn(e) {
+  const dropdown = document.getElementById("priority-dropdown");
+  dropdown.classList.toggle("show");
+}
+
+function StatusDropDownBtn(e) {
+  const dropdown = document.getElementById("status-dropdown");
+  dropdown.classList.toggle("show");
+}
+
+ddButtons.forEach(function (btns) {
+  btns.addEventListener("click", (e) => {
+    const priorityButtonId = e.target;
+
+    const sourceStyle = getComputedStyle(priorityButtonId);
+    const backgroundColor = sourceStyle.backgroundColor;
+
+    taskPriorityButton.style.backgroundColor = backgroundColor;
+
+    taskPriorityButton.innerHTML = priorityButtonId.innerHTML;
+
+    PriorityDropDownBtn();
+  });
+});
+
+ddStatusButtons.forEach(function (btns) {
+  btns.addEventListener("click", (e) => {
+    const statusButtonId = e.target;
+
+    const sourceStyle = getComputedStyle(statusButtonId);
+    const backgroundColor = sourceStyle.backgroundColor;
+
+    taskStatusButton.style.backgroundColor = backgroundColor;
+
+    taskStatusButton.innerHTML = statusButtonId.innerHTML;
+
+    StatusDropDownBtn();
+  });
+});
+
+window.onclick = function (event) {
+  if (!event.target.matches("#task-priority-button")) {
+    var sharedowns = document.getElementsByClassName("prioritydd");
+    var i;
+    for (i = 0; i < sharedowns.length; i++) {
+      var openSharedown = sharedowns[i];
+      if (openSharedown.classList.contains("show")) {
+        openSharedown.classList.remove("show");
+      }
+    }
+  }
+  if (!event.target.matches("#task-status-button")) {
+    var sharedowns = document.getElementsByClassName("statusdd");
+    var i;
+    for (i = 0; i < sharedowns.length; i++) {
+      var openSharedown = sharedowns[i];
+      if (openSharedown.classList.contains("show")) {
+        openSharedown.classList.remove("show");
+      }
+    }
+  }
+};
+
+document.getElementById("priority-dropdown")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+
+document.getElementById("status-dropdown")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
